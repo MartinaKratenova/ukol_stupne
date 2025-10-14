1141a3171fe096bbcef9202fb3df1cc23a207a99
MartinaKratenova <martina.tokova@gmail.com>
2025-10-14 18:28:33 +0200

    init

---
+++ script.js
@@ -0,0 +1,7 @@
+let fahrenheit = prompt('Zadej stupne fahreiheita:');
+
+let celsius = ((fahrenheit - 32) * 5) / 9;
+
+
+document.body.innerHTML += fahrenheit + ' stupňů fahrenheita je stupňů ' + celsius.toFixed(2) + ' celsia.';
+docume;
